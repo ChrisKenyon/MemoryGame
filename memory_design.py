165110752c7e23aa79ed5c0d16970e64192b312e
Chris Kenyon <cjkenyon117@gmail.com>
2017-05-06 11:50:21 -0400

    Miranda's file

--- memory_design.py
+++ memory_design.py
@@ -31,24 +31,23 @@ class Card:
 
         self.solved = False
         self.flipped = False
+
     def draw_card(self,screen):
         draw_x = self.posn.x * (CARD_WIDTH+CARD_BORDER_WIDTH)
         draw_y = self.posn.y * (CARD_HEIGHT+CARD_BORDER_WIDTH)
-        if self.solved:
-            pygame.draw.rect(screen, SOLVED_COLOR, (draw_x,draw_y,CARD_HEIGHT,CARD_WIDTH))
-        elif not self.flipped:
-            pygame.draw.rect(screen, HIDDEN_COLOR, (draw_x,draw_y,CARD_HEIGHT,CARD_WIDTH))
-        elif self.flipped:
-            pygame.draw.rect(screen, FLIPPED_COLOR, (draw_x,draw_y,CARD_HEIGHT,CARD_WIDTH))
-            label = card_font.render(str(self.value), 1, (255,255,0))
-            center_x = draw_x + CARD_WIDTH/2 - CARD_TEXT_SIZE/2
-            center_y = draw_y + CARD_HEIGHT/2 - CARD_TEXT_SIZE/2
-            screen.blit(label, (center_x, center_y))
+
+        # WRITE CODE HERE
+
+        # what should you draw if self.solved? (use pygame.draw.rect)
+
+        # what should you draw if not self.flipped? (use pygame.draw.rect)
+
+        # what should you draw if self.flipped? (use pygame.draw.rect and screen.blit for the number)
 
 class Grid:
     def __init__(self, size, screen):
         self.size = size
-        self._grid = []
+        self.grid = []
         self.screen = screen
 
         self.card_is_flipped = False
@@ -57,28 +56,31 @@ class Grid:
         """ Takes a list of values that should be equal
         to size * size. Creates cards for each value
         and populates the list of lists          """
-        for column in range(self.size):
-            self._grid.append([])
-            for row in range(self.size):
-                position = Posn(column, row)
-                new_card = Card(position, values.pop())
-                self._grid[column].append(new_card)
+
+        # WRITE CODE HERE
+        # create a new card object for each spot in the grid
+        # use a for loop inside a for loop
+        # to create each card, you will need to make a Posn
+        # object too, based on the indexes from you for loops
+
     def draw_all_cards(self):
         """ This is the rendering function for the grid """
-        for column in self._grid:
-            for card in column:
-                card.draw_card(self.screen)
+
+        # WRITE CODE HERE
+        # what function should you call on every Card to draw them?
+        # (use a for loop in a for loop on self.grid)
+
     def check_all_solved(self):
-        for column in self._grid:
-            for card in column:
-                if not card.solved:
-                    return False
-        return True
+        solved = False
+        # WRITE CODE HERE
+        # See if any card isn't True for self.solved
+        return solved
+
     def update_card(self,x,y):
         """ This takes an x and y coordinate corresponding
         to the card selected on the grid and updates it
         appropriately according to the rules of Memory """
-        card = self._grid[x][y]
+        card = self.grid[x][y]
         if card.solved or card.flipped:
             # do nothing if this card is solved or flipped already
             return
@@ -108,22 +110,26 @@ class Grid:
             self.flipped_card = card
             self.card_is_flipped = True
     def draw_score(self, duration):
-        message = "Completed in {} seconds.".format(duration)
-        label = msg_font.render(message, 1, (255,255,255))
-        center_x = SCREEN_WIDTH/10
-        center_y = SCREEN_HEIGHT/2 - MSG_TEXT_SIZE/2
-        self.screen.blit(label, (center_x, center_y))
+
+        # WRITE CODE HERE
+        # Make a message that says the game is over and how long you took to finish
+
         pygame.display.update()
 
 def get_random_values(grid_size):
-    num_elements = grid_size**2
-    max_value = grid_size*10 # adjust later for difficulty
-    values = []
-    for i in range(num_elements/2):
-        new_val = random.randint(1,max_value)
-        values.append(new_val)
-        # append twice to account for match
-        values.append(new_val)
+    """ This function returns a list of random integers, where
+        each number has a matching pair number. This represents
+        the values you will flip, which must have a matching pair
+        For example, with a grid size of 4, you may return something like:
+            [5, 15, 30, 23,  28, 5, 60, 15,  44, 44, 56, 23,  30, 28, 60, 56
+        where you are filling a 4x4 grid with values that all have
+        another value the same (i.e. there are 2 5's, 2 15's, ...) """
+
+
+    # WRITE CODE HERE
+
+
+    # you probably want to use this at the end
     random.shuffle(values)
     return values
 
@@ -156,16 +162,20 @@ def run_pygame_loop(game_board):
 
 def run_memory_game(grid_size):
     screen = pygame.display.set_mode((1000,1000))
-    values = get_random_values(grid_size)
-    game_board = Grid(grid_size, screen)
-    game_board.populate_grid(values)
-    duration = run_pygame_loop(game_board)
-    game_board.draw_score(duration)
+
+    # WRITE CODE HERE
+    # 1. create your list of values
+    # 2. create you Grid game board using the grid_size and screen
+    # 3. populate the grid with the values
+    # 4. run the game, which will return a duration that you should use
+    # 5. draw the score using that duration
+
     time.sleep(10)
 
 if __name__=="__main__":
     if NUMBER_CARDS % 2 != 0:
         raise Exception("Game board size must be divisible by 2 to make matches!")
+    # Initialize pygame stuff
     pygame.init()
     pygame.display.set_caption("Memory")
     card_font = pygame.font.SysFont("monospace", CARD_TEXT_SIZE)
